7f7320b67d3331eafa16b45b8cf909b97903ad9e
ThomasRascon <116411651+ThomasRascon@users.noreply.github.com>
2023-04-08 16:38:15 -0700

    Update ZeldaMap.hpp

--- ZeldaMap.hpp
+++ ZeldaMap.hpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <ostream>
 #include <vector>
-#include "Helper.hpp"
-#include "HCTree.hpp"
+#include <map>
 
 using namespace std;
 
@@ -11,16 +10,19 @@ struct GameState {
   int p2;
   int wolf;
   bool connected;
+  bool winningPath;
   vector<GameState> neighboors;
   GameState(int p1, int p2, int wolf) {
     this.p1 = p1;
     this.p2 = p2;
     this.wolf = wolf;
     this.connected = false;
+    this.winningPath = false;
 };
 
 class GameMap {
   public:
+    map<int, GameState> gameMap;
   
   private:
   
