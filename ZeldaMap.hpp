837116ad3bbd21e4747c2ddbcb53bd83ccc929be
U-LAPTOP-C27UM0Q2\chris <chrispatino6600@gmail.com>
2023-04-08 23:23:36 -0700

    Update ZeldaMap.hpp

--- ZeldaMap.hpp
+++ ZeldaMap.hpp
@@ -9,9 +9,12 @@ struct GameState {
   int p1;
   int p2;
   int wolf;
-  bool connected;
-  bool winningPath;
-  vector<GameState> neighboors;
+  bool connected; // whether or not connected to initial state
+  bool winningPath; // whether connected intial and final state
+  bool hasBeenVisited;
+  vector<char> validMoves; // all possible movements from current state
+  vector<GameState> neighboors; // possible neighbors, corresponds to validMoves
+  vector<int> winningNeighbors; // which neighbors have a path to final state, index of neighbors 
   GameState(int p1, int p2, int wolf) {
     this.p1 = p1;
     this.p2 = p2;
@@ -20,10 +23,44 @@ struct GameState {
     this.winningPath = false;
 };
 
+
+// all gamestates 
 class GameMap {
   public:
-    map<int, GameState> gameMap;
+    
+    // Recursively find path from initial to final states
+    // FinalState has arbitrary wolf position
+    vector<int> FindPath(GameState InitialState, GameState FinalState, ) {
+    
+
+
+    }
+
+    // work from initial state
+    // iterate through 4 possible directions, asking ValidMove if each direction is possible
+    // for all possible moves:
+      // check if possible moves have been created, if not create them
+      // if possible move has not been visited, run GenerateNeighbors on it
+    vector<GameState> GenerateNeighbors(GameState CurrentState) {
+
+    }
+
+
+
+    // uses configuration, which is a global variable and game rules to return whether
+    // you can go from CurrentState to direction up, down, left, or right, specified 
+    // by u, d, l, r
+    bool ValidMove(GameState CurrentState, char move) {
+
+    }
   
   private:
-  
+
+    map<int, GameState> gameMap;
+
+    vector<int> configuration // board configuration
+    
+    // dimensions of configuration
+    int length
+    int width 
 }
