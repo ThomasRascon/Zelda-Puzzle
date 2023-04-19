1c4d3b63e47ceb781f46e87d8167c36cc972a46b
Thomas Rascon <trascon@ucsd.edu>
2023-04-19 15:58:51 -0700

    added GraphTester.cpp to test our ZeldaGraph code

---
+++ GraphTester.cpp
@@ -0,0 +1,18 @@
+#include "ZeldaGraph.hpp"
+#include "BoardReader.cpp"
+
+using namespace std;
+
+int main() {
+    vector<vector<int>> board = readBoard("board1.txt");
+    vector<int> targets = findTargets(board);
+    if(targets.size() != 4){
+        cout << "Invalid input, exiting...\n";
+        exit(1);
+    }
+    pair<int,int> target_1 = {targets[0], targets[1]};
+    pair<int,int> target_2 = {targets[2], targets[3]};
+
+    GameGraph* graph = new GameGraph(board, target_1, target_2);
+    graph->build();
+}
