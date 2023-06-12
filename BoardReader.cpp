758b6c65f94626d9b9c05c8fa4a286096b9b307b
Thomas Rascon <trascon@ucsd.edu>
2023-06-12 11:31:18 -0700

    Updated randomBoard to account for cases when we have selected less than 2 spaces

--- BoardReader.cpp
+++ BoardReader.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <vector>
 #include <random>
+#include <unordered_set>
 
 using namespace std;
 
@@ -56,29 +57,31 @@ pair<pair<int,int>, pair<int,int>> findTargets(vector<vector<int>> board) {
 vector<vector<int>> randomBoard(int width, int height, double density) {
 
     vector<vector<int>> board = vector<vector<int>>(height, vector<int>(width, 0));
+    unordered_set<int> spaces;
+    int numSpaces = 0;
     for(int row = 0; row < height; ++row){
         for(int col = 0; col < width; ++col){
-            int binarynum = 0;
             double rndDouble = (double)rand() / RAND_MAX;
             if(rndDouble < density){
-                binarynum = 1;
+                board[row][col] = 1;
+                spaces.insert(row*width+col);
+                numSpaces++;
             }
-            board[row][col] = binarynum;
         }
     }
 
-    int t1_row = rand() % height;
-    int t1_col = rand() % width;
-    int t2_row = rand() % height;
-    int t2_col = rand() % width;
-
-    while(t1_row == t2_row && t1_col == t2_col){
-        t2_row = rand() % height;
-        t2_col = rand() % width;
+    if(numSpaces < 2){
+        board[0][0] = 2;
+        board[0][1] = 2;
+        return board;
     }
 
-    board[t1_row][t1_col] = 2;
-    board[t2_row][t2_col] = 2;
+    auto it = spaces.begin();
+    advance(it, rand() % spaces.size());
+    board[*it/width][*it%width] = 2;
+    spaces.erase(it);
+    advance(it, rand() % spaces.size());
+    board[*it/width][*it%width] = 2;
 
     return board;
 }//EOF randomBoard
