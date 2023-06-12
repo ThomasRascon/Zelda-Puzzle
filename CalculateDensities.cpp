c674f0f2581f1571243bb5ee58fb4337e96c3f90
U-LAPTOP-C27UM0Q2\chris <chrispatino6600@gmail.com>
2023-06-12 12:13:51 -0700

    edited GenerateDensityData.cpp and added problematic board

--- CalculateDensities.cpp
+++ CalculateDensities.cpp
@@ -1,15 +1,20 @@
 #include <ctime>
 #include "SolutionDensity.cpp"
+#include <fstream>
 
 int main(int argc, char* argv[]){
+    
+    // arguments
     int width;
     int height;
     int iterations;
     double density;
+    string outputFile;
+    
     srand(time(0));
 
     if(argc != 5){
-        cout << "Please specify a width, height, probability density, and number of iterations." << endl;
+        cout << "Please specify a width, height, probability density, number of iterations, and output file name." << endl;
         exit(1);
     }
 
@@ -18,24 +23,28 @@ int main(int argc, char* argv[]){
         height = stoi(argv[2]);
         density = stod(argv[3]);
         iterations = stoi(argv[4]);
+        outputFile = argv[5];
     } catch (const exception&){
         cout << "Please enter the correct types." << endl;
         exit(1);
     }
 
+    ofstream o;
+    o.open("OutputFiles/"+outputFile);
+
     for(int i = 0; i < iterations; ++i){
         vector<vector<int>> board = randomBoard(width, height, density);
 
-        cout << endl << "Board " << i+1 << ":"<< endl;
+        o << endl << "Board " << i+1 << ":"<< endl;
         for(int row = 0; row < height; ++row){
             for(int col = 0; col < width; ++col){
-                cout << board[row][col] << " ";
+                o << board[row][col] << " ";
             }
-            cout << endl;
+            o << endl;
         }
-        cout << endl;
+        o << endl;
 
-        cout << "Density: " << SolutionDensity(board) << endl;
+        o << "Density: " << SolutionDensity(board) << endl;
 
     }//EOF iteration loop
 
