f47b354e18bc550fc75f4409c362b4cffc58a7d1
U-LAPTOP-C27UM0Q2\chris <chrispatino6600@gmail.com>
2023-06-12 12:17:49 -0700

    edited GenerateDensityData.cpp and added problematic board

--- GraphTester.cpp
+++ GraphTester.cpp
@@ -1,9 +1,10 @@
-#include "BoardReader.cpp"
 #include "SolutionDensity.cpp"
 
 using namespace std;
 
 int main() {
+
     vector<vector<int>> board = readBoard("BoardConfigs/board1.txt");
     SolutionDensity(board);
+    
 }
