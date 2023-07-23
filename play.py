4c9095cda2f27c0e6231661a1d24d7b7d445a126
romanov360 <romanov360@gmail.com>
2023-07-23 07:56:02 -0500

    Progress on Undo Move functionality (click U)
    Works for the last move you made, but for undoing moves before that you have to click twice.

--- play.py
+++ play.py
@@ -6,7 +6,7 @@ class PuzzleGame:
         self.board = board
         self.target_positions = self.find_targets()
         self.wolf_pos, self.piece1_pos, self.piece2_pos = initial_positions
-        self.path = []
+        self.path = deque()  # Use a deque to store the move history
 
         self.root = tk.Tk()
         self.canvas = tk.Canvas(self.root, width=len(self.board[0]) * 80, height=len(self.board) * 80)
@@ -16,6 +16,7 @@ class PuzzleGame:
         self.root.bind('<Down>', self.move_wolf_down)
         self.root.bind('<Left>', self.move_wolf_left)
         self.root.bind('<Right>', self.move_wolf_right)
+        self.root.bind('u', self.undo_move)  # Bind the 'U' key to undo_move
         self.root.mainloop()
 
     def find_targets(self):
@@ -49,6 +50,8 @@ class PuzzleGame:
 
     def move_wolf(self, new_pos):
         if self.is_valid_move(new_pos):
+            current_positions = (self.wolf_pos, self.piece1_pos, self.piece2_pos)
+            self.path.append(current_positions)
             new_piece1_pos = self.get_new_position(self.piece1_pos, self.wolf_pos, new_pos, same_direction=True)
             new_piece2_pos = self.get_new_position(self.piece2_pos, self.wolf_pos, new_pos, same_direction=False)
 
@@ -67,7 +70,7 @@ class PuzzleGame:
                 self.wolf_pos = new_pos
                 self.draw_board()
                 if (self.piece1_pos, self.piece2_pos) == self.target_positions or (self.piece2_pos, self.piece1_pos) == self.target_positions:
-                    self.canvas.create_text(200, 200, text="Congratulations! You won!", font=('Arial', 24, 'bold'))
+                    self.canvas.create_text(200, 200, text="You win", font=('Arial', 24, 'bold'))
 
     def wolf_and_piece2_will_overlap(self, new_pos):
         movement=(new_pos[0]-self.wolf_pos[0],new_pos[1]-self.wolf_pos[1])
@@ -78,6 +81,31 @@ class PuzzleGame:
         message_obj = self.canvas.create_text(200, 200, text=message, font=('Arial', 24, 'bold'))
         self.root.after(duration, lambda: self.canvas.delete(message_obj))
 
+    def undo_move(self, event):
+        if not self.path:
+            self.display_message("Can't undo: beginning", duration=1000)
+        else:
+            last_positions = self.path.pop()  # Get the last recorded positions
+            if len(last_positions) == 3:  # Check if the history contains all positions
+                self.wolf_pos, self.piece1_pos, self.piece2_pos = last_positions  # Revert all positions
+            else:
+                self.display_message("Can't undo: beginning", duration=1000)
+            self.draw_board()  # Redraw the board after undoing the moves
+            self.display_message("UNDO", duration=1000)
+
+    def get_move_symbol(self, move):
+        # Helper method to convert move positions to move symbols (U, D, L, R)
+        if move == (self.wolf_pos[0] - 1, self.wolf_pos[1]):
+            return "U"
+        elif move == (self.wolf_pos[0] + 1, self.wolf_pos[1]):
+            return "D"
+        elif move == (self.wolf_pos[0], self.wolf_pos[1] - 1):
+            return "L"
+        elif move == (self.wolf_pos[0], self.wolf_pos[1] + 1):
+            return "R"
+        else:
+            return ""
+        
     def move_wolf_up(self, event):
         self.path.append(self.wolf_pos)
         new_pos = (self.wolf_pos[0] - 1, self.wolf_pos[1])
